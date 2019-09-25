a375b6f1f703657c066775e6856de7834db1e5ca
jiangyj <jij000@gmail.com>
2019-09-25 11:00:08 -0500

    v9

--- src/main/java/edu/mum/cs/filter/AllFilter.java
+++ src/main/java/edu/mum/cs/filter/AllFilter.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebFilter(urlPatterns = {"/*"})
+@WebFilter(urlPatterns = {"/ssssssss*"})
 public class AllFilter implements Filter {
 
     @Override
