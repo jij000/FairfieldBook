e2f33f750a2e62be445f67e1e9d91bee17c4214d
Xiangkui Guo <bruce1988cn@gmail.com>
2019-09-25 21:59:12 -0500

    get posts method post

--- src/main/webapp/js/global.js
+++ src/main/webapp/js/global.js
@@ -26,8 +26,24 @@ function visitOthers(id){
     window.open ("MainControllerServlet?id="+id);
 }
 
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1,c.length);
+        }
+        if (c.indexOf(nameEQ) === 0) {
+            return c.substring(nameEQ.length,c.length);
+        }
+    }
+    return null;
+}
+
 function isSelf(){
-    return 
+
+    return globalUserId==null||readCookie("userId")==globalUserId;
 }
 
 function getUrlParam(name) {
@@ -37,7 +53,7 @@ function getUrlParam(name) {
 }
 
 function followOther(id){
-    $.get("FollowController",
+    $.post("FollowController",
         {"id":id}).done(function(data){
             console.log(data);
             loadProfile();
@@ -48,9 +64,10 @@ function followOther(id){
 //start from here
 $(function(){
     globalUserId = getUrlParam("id");
-    if(globalUserId){
+    if(!isSelf()){
         $("#editorPanel").hide();
     }
     weather.init();
     posts.init();
+    isSelf();
 });
