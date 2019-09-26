f3c14bb3463ff5c4ac9c51bb73808c84118823d1
ruanxingke <rxingke@gmail.com>
2019-09-25 21:18:09 -0500

    222

--- src/main/java/edu/mum/cs/controller/ProfileController.java
+++ src/main/java/edu/mum/cs/controller/ProfileController.java
@@ -23,25 +23,25 @@ public class ProfileController extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Writer out = response.getWriter();
-
-        String name = request.getParameter("name");
+        String sId = request.getParameter("id");
+        int id=0;
+        if(sId==null || sId==""){
+            User u= (User)request.getSession().getAttribute("user");
+            id=u.getId();
+        }else{
+            id=Integer.parseInt(sId);
+        }
         User user=null;
-        if(name==null||name==""){
-            user=(User)request.getSession().getAttribute("user");
-        }
-        else
-        {
-            System.out.println("comin ...");
-            EntityManager em = FBUtility.getEntityManager(request.getServletContext());
-            TypedQuery<User> tq = em.createQuery("from User where name=?0 ",User.class);
-            tq.setParameter(0,name);
-            List<User> list = tq.getResultList();
-            if(list.size()!=0)
-                user = list.get(0);
-            user.setFollowerNum(user.getFollowerUserList().size());
-            user.setFollowingNum(user.getFollowingUserList().size());
-            em.close();
-        }
+        System.out.println("comin ...");
+        EntityManager em = FBUtility.getEntityManager(request.getServletContext());
+        TypedQuery<User> tq = em.createQuery("from User where id=?0",User.class);
+        tq.setParameter(0,id);
+        List<User> list = tq.getResultList();
+        if(list.size()!=0)
+            user = list.get(0);
+        user.setFollowerNum(user.getFollowerUserList().size());
+        user.setFollowingNum(user.getFollowingUserList().size());
+        em.close();
 
         final GsonBuilder builder = new GsonBuilder();
         builder.excludeFieldsWithoutExposeAnnotation();
@@ -49,6 +49,7 @@ public class ProfileController extends HttpServlet {
         final Gson gson = builder.create();
 
         String jsonStr  = gson.toJson(user);
+        System.out.println(jsonStr);
         out.write(jsonStr);
     }
 }
